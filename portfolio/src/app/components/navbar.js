1e6cf920ffb9553bd965648f61121f730636d3cc
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2025-05-12 15:24:35 -0700

    Fix responsiveness for both navigation bars

--- portfolio/src/app/components/navbar.js
+++ portfolio/src/app/components/navbar.js
@@ -1,39 +1,35 @@
-import { useState } from 'react';
-import '../globals.css'; 
- 
-export default function NavBar(){
-    const [Title,setTitle] = useState("Blog")
-    
-return(<>
+"use client";
 
-        <section className = "OverContainer">
+import { useState } from "react";
+import "../globals.css";
 
-            <button id = "JohnGutierrez" className = "Nav_Buttons_1">
-                John Gutierrez
-            </button>
-            
-            <button id = "Projects" className = "Nav_Buttons_1">
-                Projects
-            </button>
-            <div id="SearchBar">
-  <img src="search-icon.png" alt="Search Icon" />
-  <input
-    type="text"
-    placeholder="Search..."
-    className="SearchProjects"
-    aria-label="Search Projects"
-  />
-</div>
+export default function NavBar() {
 
-            <section className ="PostingContainer">
-                <button id = "Blogs" className = "Nav_Buttons_1">
-                    Blogs
-                </button>
-                <button id = "Github" className = "Nav_Buttons_1">Github</button>
-                </section>
-            <button id = "ContactPage" className = "Nav_Buttons_1">
-                Contact Page
-            </button>
-        </section>
-</>);
+  return (
+    <section className="OverContainer">
+      <button id="JohnGutierrez" className="Nav_Buttons_1">
+        John Gutierrez
+      </button>
+
+      <button id="Projects" className="Nav_Buttons_1">
+        Projects
+      </button>
+
+      <div id="SearchBar">
+        <img src="search-icon.png" alt="Search Icon" />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="SearchProjects"
+          aria-label="Search Projects"
+        />
+      </div>
+
+      <button id="ContactPage" className="Nav_Buttons_1">
+        Contact Page
+      </button>
+        <button id = "CurrencyTop" className="Nav_Buttons_1">Projects Completed:5</button>
+    </section>
+   
+  );
 }
---
+++ portfolio/src/app/components/navbar2.js
@@ -0,0 +1,22 @@
+"use client";
+import { useState } from "react";
+import "../navbar2.css"
+export default function NavBar2(){
+    return(
+        <section id = "Navbar2Section">
+          
+            <button><img />John Gutierrez</button>
+            <button>Profile </button>
+            <button>Messages</button>
+            <button>Friends</button>
+            <button>Inventory</button>
+            <button>Trade</button>
+            <button>Communities</button>
+            <button>Blog</button>
+            <button>Github</button>
+            
+    
+        </section>
+    );
+
+}
