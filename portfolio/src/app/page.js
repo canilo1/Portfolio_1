ec164466952208db76ac21004e751b540595774d
John Gutierrez <75353216+canilo1@users.noreply.github.com>
2025-05-23 14:39:10 -0700

    ProfileBranch

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,10 +1,13 @@
 import NavBar from "./components/navbar";
 import NavBar2 from "./components/navbar2";
+
 export default function Home() {
   return (
-    <>
+    <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
       <NavBar />
-      <NavBar2 />
-    </>
+      <div style={{ display: "flex", flex: 1, overflow: "hidden" }}>
+        <NavBar2 />
+      </div>
+    </div>
   );
 }
